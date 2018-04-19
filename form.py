4897a09c0ef988fbfce24975edd68ab280aaf92f
Ajay Kumar <kajay5080@gmail.com>
2018-04-19 16:09:47 +0530

    added qr code generation method

--- form.py
+++
@@ -1,8 +0,0 @@
-from flask_wtf import Form
-from wtforms import TextField, IntegerField, TextAreaField, SubmitField, RadioField, SelectField
-
-from wtforms import validators, ValidationError
-
-class QRForm(Form):
-    texts = TextField("Enter medicine name", [validators.Required("Please enter medicine name!!!")])
-    submit = SubmitField("GO")
