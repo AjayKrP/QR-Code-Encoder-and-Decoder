3e4180c17e09511c4296f1391f89b70ef8ab8295
ajay <23704820+AjayKrP@users.noreply.github.com>
2018-04-24 10:37:59 +0530

    Update QRGenerate.py

--- QRGenerate.py
+++ QRGenerate.py
@@ -5,6 +5,8 @@ import os
 def generate_qr_code(text, id):
     path = os.getcwd()
     print(path)
+    if not os.path.exists(path+'/static/images'):
+        os.makedirs(path+'/static/images')
     os.chdir(path+'/static/images')
     image = pyqrcode.create(text)
     image.png(id+'.png', scale=10*2)
